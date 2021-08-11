af43655936923a228da87fda42940f6c7372ef12
GuhMatoso <88804536+GuhMatoso@users.noreply.github.com>
2021-08-11 20:16:20 -0300

    Update Principal2.java
    Correção nos comentários, linha 24.

--- Meu primeiro programa JAVA/src/meuPrimeiroProgramaJAVA/Principal2.java
+++ Meu primeiro programa JAVA/src/meuPrimeiroProgramaJAVA/Principal2.java
@@ -9,19 +9,19 @@ import java.util.Scanner;
  * @author Rafael
  *
  */
-public class Principal2 { // Classe que mantÈm o mÈtodo principal.
+public class Principal2 { // Classe que mant√©m o m√©todo principal.
 
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) { // Este È o modo principal.
-		System.out.println("Ol· amigo!\nQual È o seu nome?"); // print usado para pular linha "println"
+	public static void main(String[] args) { // Este √© o modo principal.
+		System.out.println("Ol√° amigo!\nQual √© o seu nome?"); // print usado para pular linha "println"
 		// Ler uma string do console usando a biblioteca scanner
 		Scanner in = new Scanner(System.in);
 		String nome = in.nextLine();
-		System.out.printf("Ol· %s!\n", nome);
+		System.out.printf("Ol√° %s!\n", nome);
 		
-		// Coment·rio para teste para do github..
+		// Coment√°rio para teste do github..
 		
 
 	}
