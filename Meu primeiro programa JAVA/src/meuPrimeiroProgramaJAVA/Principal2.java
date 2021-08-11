9b18f35211f7495eef29cfe9aa7ab44d2cb7ed8b
GuhMatoso <88804536+GuhMatoso@users.noreply.github.com>
2021-08-11 19:34:47 -0300

    Teste
    Teste

---
+++ Meu primeiro programa JAVA/src/meuPrimeiroProgramaJAVA/Principal2.java
@@ -0,0 +1,27 @@
+/*Meu primeiro programa java*
+ * 
+ */
+package meuPrimeiroProgramaJAVA;
+
+import java.util.Scanner;
+
+/**
+ * @author Rafael
+ *
+ */
+public class Principal2 { // Classe que mantém o método principal.
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) { // Este é o modo principal.
+		System.out.println("Olá amigo!\nQual é o seu nome?"); // print usado para pular linha "println"
+		// Ler uma string do console usando a biblioteca scanner
+		Scanner in = new Scanner(System.in);
+		String nome = in.nextLine();
+		System.out.printf("Olá %s!\n", nome);
+		
+
+	}
+
+}
