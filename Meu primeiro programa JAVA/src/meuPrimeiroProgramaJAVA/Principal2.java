0e6c7838b7b264c8d992217fc3d8a55b50010db3
Rafael <Rafael@RAFAEL>
2021-08-11 21:35:24 -0300

    Foi inserido ajustes de assentuações.

--- Meu primeiro programa JAVA/src/meuPrimeiroProgramaJAVA/Principal2.java
+++ Meu primeiro programa JAVA/src/meuPrimeiroProgramaJAVA/Principal2.java
@@ -9,14 +9,14 @@ import java.util.Scanner;
  * @author Rafael
  *
  */
-public class Principal2 { // Classe que mant√©m o m√©todo principal.
+public class Principal2 { // Classe que mantÈm o todo principal.
 
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) { // Este √© o modo principal.
+	public static void main(String[] args) { // Este È o modo principal.
 		System.out.println("Ol·! amigo!\nQual È o seu nome?"); // print usado para pular linha "println"
-		// Ler uma string do console usando a biblioteca scanner
+		// Ler uma string do console usando a biblioteca scanner.
 		Scanner in = new Scanner(System.in);
 		String nome = in.nextLine();
 		System.out.printf("Ol·! %s!\n", nome);
@@ -27,7 +27,7 @@ public class Principal2 { // Classe que mant√©m o m√©todo principal.
 		
 		System.out.printf("%d! Puxa! Como vocÍ È jovem!", idade);
 		
-		// Coment√°rio para teste do github..
+		// Coment·rio para teste do github.
 		
 
 	}
