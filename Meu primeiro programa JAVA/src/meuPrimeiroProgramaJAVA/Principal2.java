e8b7f60511e04d3d96645f94e8f8634007d5a1e7
Rafael <Rafael@RAFAEL>
2021-08-11 21:27:59 -0300

    Foi adicionado uma pergunta sobre a idade do usuário.

--- Meu primeiro programa JAVA/src/meuPrimeiroProgramaJAVA/Principal2.java
+++ Meu primeiro programa JAVA/src/meuPrimeiroProgramaJAVA/Principal2.java
@@ -15,11 +15,17 @@ public class Principal2 { // Classe que mant√©m o m√©todo principal.
 	 * @param args
 	 */
 	public static void main(String[] args) { // Este √© o modo principal.
-		System.out.println("Ol√° amigo!\nQual √© o seu nome?"); // print usado para pular linha "println"
+		System.out.println("Ol·! amigo!\nQual È o seu nome?"); // print usado para pular linha "println"
 		// Ler uma string do console usando a biblioteca scanner
 		Scanner in = new Scanner(System.in);
 		String nome = in.nextLine();
-		System.out.printf("Ol√° %s!\n", nome);
+		System.out.printf("Ol·! %s!\n", nome);
+		
+		System.out.printf("Qual a sua idade ?");
+		
+		Integer idade = in.nextInt();
+		
+		System.out.printf("%d! Puxa! Como vocÍ È jovem!", idade);
 		
 		// Coment√°rio para teste do github..
 		
